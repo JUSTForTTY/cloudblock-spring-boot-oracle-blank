5315990ab7d9133563c6a81c1a43b57424c445c0
JUSTForTTY <1547970885@qq.com>
2019-05-25 15:56:07 +0800

    zzx

--- src/main/java/com/company/project/configurer/MybatisConfigurer.java
+++ src/main/java/com/company/project/configurer/MybatisConfigurer.java
@@ -51,7 +51,6 @@ public class MybatisConfigurer {
   }
 
 
-
   @Bean(name = "sqlSessionFactoryOne")
   @Primary
   public SqlSessionFactory sqlSessionFactoryOne(@Qualifier("dataSourceOne") DataSource dataSource)throws Exception{
@@ -67,10 +66,10 @@ public class MybatisConfigurer {
       properties.setProperty("reasonable", "true");//页码<=0 查询第一页，页码>=总页数查询最后一页
       properties.setProperty("supportMethodsArguments", "true");//支持通过 Mapper 接口参数来传递分页参数
       pageHelper.setProperties(properties);
-
+      
       //添加插件
       factory.setPlugins(new Interceptor[]{pageHelper});
-
+      
       //添加XML目录
       ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
       factory.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
